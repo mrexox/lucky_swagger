d622ef2a65501cbe7a505311d34b341c8bd640c4
Kiselev Valentine <mrexox@outlook.com>
2020-01-20 09:25:06 +0300

    Compiling only if ENV variable provided (internal)

--- src/lucky_swagger/api_action.cr
+++ src/lucky_swagger/api_action.cr
@@ -3,22 +3,29 @@ require "./action_object"
 module LuckySwagger
   # Include this module in src/actions/api_action.cr
   module ApiAction
-    private macro define_action(method)
+    private macro swagger_method(method)
       def self.{{method.id}}(uri : String, description : String, params : NamedTuple? = nil, &block)
-        parameters = params || NamedTuple.new
+        {% if env("LUCKY_SWAGGER_ENABLED") %}
+          parameters = params || NamedTuple.new
 
-        with build_action(
-          {{method}},
-          parameters.to_h.merge(
-            {
-              :uri => uri,
-              :description => description,
-              :authorization => true
-            }
-          )
-        ) yield
+          with build_action(
+               {{method}},
+               parameters.to_h.merge(
+                 {
+                   :uri => uri,
+                   :description => description,
+                   :authorization => true
+                 }
+               )
+          ) yield
+        {% else %}
+          # Do nothing
+        {% end %}
       end
     end
+
+
+
 
     macro included
       @@action_store = [] of LuckySwagger::ActionObject
@@ -29,11 +36,11 @@ module LuckySwagger
         action
       end
 
-      define_action :get
-      define_action :post
-      define_action :patch
-      define_action :put
-      define_action :delete
+      swagger_method :get
+      swagger_method :post
+      swagger_method :patch
+      swagger_method :put
+      swagger_method :delete
     end
   end
 end
