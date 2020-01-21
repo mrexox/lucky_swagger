517bf64ccb0b458b4fa70f3003136498704d8781
Ian <mrexox@outlook.com>
2020-01-21 20:59:35 +0300

    Added config example

--- src/lucky_swagger/config.cr
+++ src/lucky_swagger/config.cr
@@ -1,7 +1,17 @@
 require "habitat"
 
 module LuckySwagger
-  # TODO: add examples
+
+  # Example:
+  # LuckySwagger.config do |config|
+  #  config.title = "My API application"
+  #  config.version = ENV["APP_VERSION"]
+  #  config.description = "Application description"
+  #  config.terms_url = "https://github.com/me/project/TERMS_OF_USE"
+  #  config.license = {"MIT", "https://github.com/me/project/LICENSE"}
+  #  config.contact = {"me", "mymail@mail.ru", "mysite.com"}
+  # end
+
   Habitat.create do
     setting title : String = "Swagger"
     setting version : String
@@ -9,6 +19,8 @@ module LuckySwagger
     setting terms_url : String?
     setting license : {String, String?}
     setting contact : {String, String?}
-    setting authorizations : Array(String)
+
+    # Authorization is not supported yet
+    # setting authorizations : Array(String)
   end
 end
